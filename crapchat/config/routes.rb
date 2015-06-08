01635698b094e97873f31a2c059af77df6e36aec
Matt Duncan <busman84@gmail.com>
2015-06-08 13:29:59 -0500

    some styling and configured the route to create.html.erb

--- crapchat/config/routes.rb
+++ crapchat/config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get 'about' => 'static_pages#about'
 
-  get 'contact' => 'static_pages#create'
+  get 'contact' => 'static_pages#contact'
+  
+  get 'create' => 'static_pages#create'
 
   get 'links' => 'static_pages#links'
 

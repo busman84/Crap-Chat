b5b24558ae54aee15f2d89466e6debee2c27d41f
daniel rendon <daniel.rendonfc@gmail.com>
2015-06-08 16:37:08 -0500

    fixed it

--- crapchat/app/controllers/users_controller.rb
+++ crapchat/app/controllers/users_controller.rb
@@ -16,15 +16,32 @@ class UsersController < ApplicationController
   
   end
 
+  # def create
+  #     @user = User.new(user_params)
+  #     respond_to do |format|
+  #     if @user.save
+  #       log_in @user
+  #       redirect_to @user
+  #     else
+  #         render 'new'
+  #     end
+  #   end
+  #   end
   def create
-      @user = User.new(user_params)
+    p params
+    @user = User.new(user_params)
+
+    respond_to do |format|
       if @user.save
-        log_in @user
-        redirect_to @user
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
       else
-          render 'new'
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+  end
+  
 
     def edit
       @user = User.find(params[:id])

f0e4334c62d219c91350242d060cd9c9cdad9cb4
daniel rendon <daniel.rendonfc@gmail.com>
2015-06-08 11:59:03 -0500

    adding actions for controller

--- crapchat/app/controllers/users_controller.rb
+++ crapchat/app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def index
+  end
+  
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+  	
+
 end

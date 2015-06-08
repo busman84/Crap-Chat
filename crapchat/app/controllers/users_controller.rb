a698849534aa101f1d7a80db9a3bc48148482fdf
Matt Duncan <busman84@gmail.com>
2015-06-08 14:15:03 -0500

    Updating some sessions info

--- crapchat/app/controllers/users_controller.rb
+++ crapchat/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+
   def new
   end
 
@@ -9,7 +12,14 @@ class UsersController < ApplicationController
   end
 
   def create
-  end
+    	@user = User.new(user_params)
+    	if @user.save
+    		log_in @user
+     		redirect_to @user
+   		else
+      		render 'new'
+    	end
+  	end
 
   def edit
   end
@@ -21,6 +31,22 @@ class UsersController < ApplicationController
   end
 
   private 
-  	
+  	def set_user
+      @user = User.find(params[:id])
+    end
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:id, :username, :email, :password, :first_name, :last_name)
+    end
+
+     # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+      	store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
 end

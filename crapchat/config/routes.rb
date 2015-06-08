6dff9b229bfe5861564ebac094f27cc5f45f5bc2
Matt Duncan <busman84@gmail.com>
2015-06-08 11:48:06 -0500

    added Users controller and Model

--- crapchat/config/routes.rb
+++ crapchat/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
 
   get 'about' => 'static_pages#about'
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
   get 'links' => 'static_pages#links'
 
   get 'help' => 'static_pages#help'
+
+  get 'signup' => 'users#new'
 
   
 end

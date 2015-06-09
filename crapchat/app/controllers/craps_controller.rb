ff8d6a766846e261cc5a39223af3404667759eda
Matt Duncan <busman84@gmail.com>
2015-06-09 11:55:28 -0500

    commiting daniels changes to craps controller

--- crapchat/app/controllers/craps_controller.rb
+++ crapchat/app/controllers/craps_controller.rb
@@ -1,33 +1,47 @@
 class CrapsController < ApplicationController
-	before_action :set_crap, only: [:show, :edit, :update, :destroy]
-	def create
-		
-	end
-
-	def edit
-		
-	end
-
-	def destroy
-		
-	end
-
-	def new
-	 @crap = Crap.new	
-	end
-
-	def show
-		
-	end
-
-	
-	private
-    def set_crap
-      @crap = Crap.find(params[:id])
+    before_action :set_crap, only: [:show, :edit, :update, :destroy]
+    def create
+        @crap = Crap.new(crap_params)
+        respond_to do |format|
+            if @crap.save
+                format.html {redirect_to @crap, notice: "Solid Crap"}
+            else 
+                format.html {render :new}
+            end    
+        end
     end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-    def crap_params
-      params.require(:crap).permit(:id, :post)
+    def edit
+        
     end
+
+    def destroy
+        Crap.find(params[:id]).destroy
+        respond_to do |format|
+            format.html{redirect_to craps_url}
+        end    
+    end
+
+    def new
+     @crap = Crap.new    
+    end
+
+    def show
+        @crap = Crap.find(params[:id])
+    end
+
+    def index
+        @craps = Crap.all
+    end
+
+    
+    private
+   def set_crap
+     @crap = Crap.find(params[:id])
+   end
+
+ # Never trust parameters from the scary internet, only allow the white list through.
+   def crap_params
+     params.require(:crap).permit(:id, :post)
+   end
 end

d286f208eebabea27751f2b9920233f1a8a31bcd
daniel rendon <daniel.rendonfc@gmail.com>
2015-06-09 11:51:05 -0500

    actions for craps

--- crapchat/app/controllers/craps_controller.rb
+++ crapchat/app/controllers/craps_controller.rb
@@ -1,7 +1,14 @@
 class CrapsController < ApplicationController
 	before_action :set_crap, only: [:show, :edit, :update, :destroy]
 	def create
-		
+		@crap = Crap.new(crap_params)
+		respond_to do |format|
+			if @crap.save
+				format.html {redirect_to @crap, notice: "Solid Crap"}
+			else 
+				format.html {render :new}
+			end	
+		end
 	end
 
 	def edit
@@ -9,7 +16,10 @@ class CrapsController < ApplicationController
 	end
 
 	def destroy
-		
+		Crap.find(params[:id]).destroy
+		respond_to do |format|
+			format.html{redirect_to craps_url}
+		end	
 	end
 
 	def new
@@ -17,7 +27,11 @@ class CrapsController < ApplicationController
 	end
 
 	def show
-		
+		@crap = Crap.find(params[:id])
+	end
+
+	def index
+		@craps = Crap.all
 	end
 
 	
